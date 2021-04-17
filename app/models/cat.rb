5ede070348d331374bdb496dae25c2ebfaaef2d2
ben20262 <ben20262@aim.com>
2021-04-17 18:28:42 -0400

    added create and delete cats function

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,16 @@
 class Cat < ApplicationRecord
+
+    def self.createCats
+        newCats = File.open('app/assets/spreadsheet.txt')
+        newCats.each do |cat|
+            catArray = cat.split(',')
+            catArray.map! {|catItem| catItem.delete "[]"}
+            self.create({make: catArray[0], serial: catArray[1], price: catArray[2].to_i})
+        end
+    end
+
+    def self.deleteCats
+        self.destroy_all
+    end
+
 end
